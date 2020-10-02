ef72d4784c0f63c92684cd18357530cb0be0e7b3
josephlunt <joey.lunt@gmail.com>
2020-10-02 14:17:36 +1000

    Request camera permission dialog and updated ui button names

--- app/src/main/java/com/example/foraa/MainActivity.java
+++ app/src/main/java/com/example/foraa/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.foraa;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
-
+@RequiresApi(api = Build.VERSION_CODES.M)
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, 100);
+        }
     }
 }
